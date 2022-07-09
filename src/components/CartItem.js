5d3cab2b778b95ee5e38ba62d6f4b654badb5dbb
Christopher Perez <chistoperez@gmail.com>
2022-07-09 16:33:57 -0500

    cart

---
+++ src/components/CartItem.js
@@ -0,0 +1,18 @@
+const CartItem = ({ data, delFromCart }) => {
+  let { id, name, price, quantity } = data;
+  return (
+    <div style={{ borderBottom: "thin solid gray" }}>
+      <h4>{name}</h4>
+      <h5>
+        ${price}.00 x {quantity} = ${price * quantity}.00
+      </h5>
+      <button onClick={() => delFromCart(id)}>Delete One</button>
+      <br />
+      <button onClick={() => delFromCart(id, true)}>Delete All </button>
+      <br />
+      <br />
+    </div>
+  );
+};
+
+export default CartItem;
